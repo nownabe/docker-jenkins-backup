c27c1e11ddd34d02b0f9c62026dcc84fc7cdb8a4
nownabe <nownabe@gmail.com>
2015-02-12 12:21:33 +0900

    modify backup.sh

--- backup.sh
+++ backup.sh
@@ -14,4 +14,17 @@ EOF
 
 [ -v AWS_S3_HOST ] && echo "aws_s3_host: ${AWS_S3_HOST}" >> ${s3config}
 
+: "Download Backups"
+/opt/s3sync/s3sync.rb -r ${BACKUP_BUCKET}:${BACKUP_PREFIX:=""} /backup
+
+: "Backup Jenkins and remove old backups"
+/usr/local/bin/jenkins-backup /jenkins /backup/jenkins-backup_`date +%Y%m%d%H%M%S`.tar.gz
+find /backup -type f \
+        -name "jenkins-backup_*.tar.gz" \
+        -daystart -mtime +${BACKUP_EXPIRE:=30} \
+        | xargs rm -f
+
+: "Sync Backups to S3"
+/opt/s3sync/s3sync.rb -r --delete /backup ${BACKUP_BUCKET}:${BACKUP_PREFIX}
+
 
